1f6e14e8411a1918de823622b1636a6a4ae492f2
yhaddar <youssefhaddar9@gmail.com>
2026-05-09 12:45:52 +0100

    Ignore config and derby log files

--- src/config.py
+++
@@ -1,8 +0,0 @@
-sfOptions = {
-  "sfURL": "KLEVWLW-WW99361.snowflakecomputing.com",
-  "sfUser": "yhaddar",
-  "sfPassword": "0654381265Haddar",
-  "sfDatabase": "uber_dw",
-  "sfSchema": "PUBLIC",
-  "sfWarehouse": "COMPUTE_WH"
-}
